9c625014e90035ee4dea95cfa2311b5c965de36a
haideraqeeb <haiderraqeeb10@gmail.com>
2024-12-11 21:45:11 +0530

    chore: initial commit

---
+++ siglip.py
@@ -0,0 +1,116 @@
+from typing import Optional, Tuple
+import torch
+import torch.nn as nn
+
+
+class SiglipVisionConfig:
+    def __init__(
+        self,
+        hidden_size=768,
+        intermediate_size=3072,
+        num_hidden_layers=12,
+        num_attention_heads=12,
+        num_channels=3,
+        image_size=224,
+        patch_size=16,
+        layer_norm_eps=1e-6,
+        attention_dropout=0.0,
+        num_image_tokens: int = None,
+        **kwargs
+    ):
+        super().__init__()
+
+        self.hidden_size = hidden_size
+        self.intermediate_size = intermediate_size
+        self.num_hidden_layers = num_hidden_layers
+        self.num_attention_heads = num_attention_heads
+        self.num_channels = num_channels
+        self.image_size = image_size
+        self.patch_size = patch_size
+        self.layer_norm_eps = layer_norm_eps
+        self.attention_dropout = attention_dropout
+        self.num_image_tokens = num_image_tokens
+
+
+class SiglipVisionEmbeddings(nn.Module):
+    def __init__(self, config: SiglipVisionConfig):
+        super().__init__()
+        self.config = config
+        self.embed_dim = config.hidden_size
+        self.image_size = config.image_size
+        self.patch_size = config.patch_size
+
+        self.patch_embedding = nn.Conv2d(
+            in_channels=config.num_channels,
+            out_channels=self.embed_dim,
+            kernel_size=self.patch_size,
+            stride=self.patch_size,
+            padding="valid"  # no padding is added
+        )
+
+        self.num_patches = (self.image_size // self.patch_size) ** 2
+        self.num_positions = self.num_patches
+        self.position_embedding = nn.Embedding(
+            self.num_positions,
+            self.embed_dim
+        )
+        self.register_buffer(
+            "position_ids",
+            torch.arrange(self.num_positions).expand((-1, 1)),
+            persistent=False
+        )
+
+    def forward(self, pixel_values: torch.FloatTensor) -> torch.Tensor:
+        # [Batch_size, Channels, Height, Width]
+        _, _, height, width = pixel_values.shape
+        # After convolution the output is of dimension [Batch_size, Embedding_Dimension, Num_Patches_H, Num_Patches_W]
+
+        patch_embeds = self.patch_embedding(pixel_values)
+
+        # [Batch_size, Embedding_Dimension, Num_Patches_H, Num_Patches_W] -> [Batch_Size, Embeddings_Dimension, Num_Patches]
+        embeddings = patch_embeds.flatten(2)
+
+        # [Batch_Size, Embedding_Dimension, Num_Patches] -> [Batch_Size, Num_Patches, Embedding_Dimension]
+        embeddings = embeddings.transpose(1, 2)
+
+        # Adding positional embeddings
+        embeddings = embeddings = self.position_embedding(self.position_ids)
+
+        return embeddings
+
+
+class SiglipVisionTransformer(nn.Module):
+    def __init__(self, config: SiglipVisionConfig):
+        super().__init__()
+
+        self.config = config
+        embed_dim = config.hidden_size
+
+        self.embeddings = SiglipVisionEmbeddings(config)
+        self.encoder = SiglipEncoder(config)
+        self.post_layernorm = nn.LayerNorm(
+            embed_dim,
+            eps=config.layer_norm_eps
+        )
+
+    def forward(self, pixel_values: torch.Tensor) -> torch.Tensor:
+        # [Pixel_Values: [Batch_Size, Channels, Height, Width] -> [Batch_Size, Num_Patches, Embedding_Dimension]]
+        hidden_states = self.embeddings(pixel_values)
+
+        last_hidden_state = self.encoder(input_embeds=hidden_states)
+
+        last_hidden_state = self.post_layernorm(last_hidden_state)
+
+        return last_hidden_state
+
+
+class SiglipVisionModel(nn.Module):
+    def __init__(self, config: SiglipVisionConfig):
+        super().__init__()
+
+        self.config = config
+        self.vision_model = SiglipVisionTransformer(config)
+
+    def forward(self, pixel_values) -> Tuple:
+        # [Batch_Size, Channels, Height, Width] -> [Batch_Size, Num_Patches, Embedding_Dimension]
+        return self.vision_model(pixel_values=pixel_values)
